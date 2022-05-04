a5cddbdaa248444be3edbfbd5fbf64f0c221db6d
Renold Kingsely <renold.k@verandalearning.com>
2022-05-04 17:08:25 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'quote-generator';
+  qouteJson: any;
+  qouteToShow:any
+  qouteIndex=0
+  authorName:any
+  
+  constructor(private http: HttpClient) {
+    this.getQuote()
+   }
+
+   getQuote() {
+    
+    this.http.get("https://type.fit/api/quotes").subscribe(data=>{
+      this.qouteJson=data 
+      this.qouteToShow=this.qouteJson[this.qouteIndex].text
+      this.authorName=this.qouteJson[this.qouteIndex].author
+      this.qouteIndex+=1
+    })
+
+   }
+
 }
